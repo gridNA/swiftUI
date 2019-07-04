6bd8ed9f46abcb1fe672ecab6a6575454116a906
Kateryna Gridina <gridina.kate17089@gmail.com>
2019-07-04 08:42:50 +0200

    simple

--- simpleViews/PlantsForm.swift
+++ simpleViews/PlantsForm.swift
@@ -9,63 +9,8 @@
 import SwiftUI
 import Combine
 
-//// 1. create a form with the name of the plant
-//
-//struct LabelTextField : View {
-//    var label: String
-//    var placeHolder: String? = ""
-//    @ObjectBinding var plant: Plant
-//
-//    var body: some View {
-//
-//        VStack(alignment: .leading) {
-//            Text(label)
-//                .font(.headline)
-//            TextField($plant.name, placeholder: Text(placeHolder ?? ""))
-//            .padding(.all)
-//            .background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0), cornerRadius: 5.0)
-//            }
-//            .padding(.horizontal, 15)
-//    }
-//}
-//
-//
-//struct PlantsForm : View {
-//    @ObjectBinding var plant: Plant
-//
-//    var body: some View {
-//        NavigationView {
-//            List {
-//                VStack(alignment: .leading) {
-//                    LabelTextField(label: "NAME", plant: plant)
-//                }
-//            }
-//        }
-//        .navigationBarTitle(Text("Plants editor"))
-//    }
-//}
-//
-//#if DEBUG
-//struct PlantsForm_Previews : PreviewProvider {
-//
-//    static var previews: some View {
-//       PlantsForm(plant: Plant())
-//    }
-//}
-//#endif
-//
-//
-//
-//
 
-
-
-
-
-
-
-
-// 2. add picker
+// create a simple form with 1 text field
 
 struct LabelTextField : View {
     var label: String
@@ -90,21 +35,7 @@ struct PlantsForm : View {
     @ObjectBinding var plant: Plant
     
     var body: some View {
-        
-        NavigationView {
-            List {
-                VStack(alignment: .leading) {
-                    LabelTextField(label: "NAME", plant: plant)
-                    Picker(selection: $plant.toxicity, label: Text("Toxicity")) {
-                        ForEach(Toxicity.allCases.identified(by: \.self)) {
-                            Text($0.rawValue).tag($0)
-                        }
-                    }
-                }
-            }
-        }
-        .navigationBarTitle(Text("Plants editor"))
-        
+        Text("")
     }
 }
 
@@ -112,7 +43,7 @@ struct PlantsForm : View {
 struct PlantsForm_Previews : PreviewProvider {
     
     static var previews: some View {
-        PlantsForm(plant: Plant())
+        PlantsForm()
     }
 }
 #endif
